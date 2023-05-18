3c5cfdab4cf721619bd05c58196d5b86e881723e
Silver Jester <auron7985@gmail.com>
2023-05-17 22:09:14 -0500

    redid code to actually use user input and did README

--- script.js
+++ script.js
@@ -1,46 +1,89 @@
+  
+ 
  // Get references to the #generate element
  var generateBtn = document.querySelector("#generate");
 
- // Create variables used in password; 
-   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-   const symbols = "~`!@#$%^&*()-_+={[}]|\:';'<>?,./";
-   const numbers = "0123456789"; 
  
- 
+   
+   
  //create function to generate password
- function generatePassword() {
+   
+   function generatePassword() {
+  //created variables for options
+   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+   var symbols = "~`!@#$%^&*()-_+={[}]|\:';'<>?,./";
+   var numbers = "0123456789"; 
    var options = "";
+   //set variable for pw length and checks for proper user input
    var passwordLength = prompt("Pick a password length from 8 to 128 characters.");
    if (passwordLength < 8 || passwordLength > 128){
-     alert("Please try again.");
-     return generatePassword();
+     return("Please try again.");
+    } else if (isNaN(passwordLength)) {
+      passwordLength = prompt("Please enter a valid number");
+    }
+    else {
+      alert("Your password will be " + passwordLength + " characters long.")
+    }
+    
+  
+   //use confirm to get user choices and confirm those choices
+   var wantUppercase = confirm("Do you want uppercase letters?");
+   if (wantUppercase) {
+    alert("Password will include uppercase characters.");
+   }else{
+    alert("Password will not include uppercase characters.")
    }
-   //use confirm to get user choices
-   var wantUppercase = confirm("Do you want uppercase letters?")
-   var wantLowercase = confirm("Do you want lowercase letters?")
+   
+
+   var wantLowercase = confirm("Do you want lowercase letters?");
+   if (wantLowercase) {
+     alert("Password will include lowercase characters.");
+   }else{
+    alert("Password will not include lowercase characters")
+   }
+
    var wantNumeric = confirm ("Do you want numbers?")
+   if (wantNumeric) {
+     alert("Password will include numbers.");
+   }else{
+     alert("Password will not include numbers.")
+   }
+
    var wantSymbols = confirm("Do you want special characters?")
-   if (!wantUppercase && !wantLowecase && !wantNumeric && !wantSymbols){alert("Must choose at least one option.")
- return generatePassword();
- }
-   if (wantUppercase) {options += upperCase
+   if (wantSymbols) {
+    alert("Password will include special characters.");
+   }else{
+    alert("Password will not include special characters.")
    }
-   if (wantLowercase) {options += lowerCase
-   }
-   if (wantNumeric) {options += numbers
-   }
-   if (wantSymbols) {options += symbols
-   }
+   // check to make sure user picks one character
+   if (wantLowercase === false && wantUppercase === false && wantNumeric === false && wantSpecial === false) {
+    return "Please select at least one character type.";
+  };
+
+  // combine selected characters
+  if (wantLowercase) {
+    options = options.concat(lowerCase);
+  }
+  if (wantUppercase) {
+    options = options.concat(upperCase);
+  }
+  if (wantNumeric) {
+    options = options.concat(numbers);
+  }
+  if (wantSymbols) {
+    options = options.concat(symbols);
+  }
+   
    var password = ""
    for ( var i = 0; i < passwordLength; i++){
      var randomIndex = Math.floor(Math.random() * options.length);
      var randomChar = options[randomIndex];
      password += randomChar;
   }
-    return password
-     }
- 
+    return password;
+     
+  };
    
  
   //Write password to the #password input
@@ -52,7 +95,8 @@
   
  }
  
- 
-  //Add event listener to generate button
+ //Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
+ 
+
  
